6242c4d7c41ceb5dfa4dedd0a0a28aa929aed51d
QuangAnh <dinhquanganhtran@gmail.com>
2026-08-27 14:08:55 +0700

    update week 4

---
+++ app.js
@@ -0,0 +1,11 @@
+alert("Hello from an embedded script")
+
+// Function declaration- equivalent to Python's def
+function square(x) {
+    return x * x;
+}
+
+// Arrow function- equivalent to a Python lambda
+const double = (x) => x * 2;
+console.log(double(5)); // Output: 10
+
